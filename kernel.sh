4e5e122bfa96dd1cb8b5b62303f95a325f0ee100
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-30 19:16:56 -0400

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -3,6 +3,4 @@
 eselect kernel list
 cd /usr/src/linux
 
-make -j12 && make modules_install && make install
-
-grub-mkconfig -o /boot/grub/grub.cfg
+make -j12 && make modules_install && make install && grub-mkconfig -o /boot/grub/grub.cfg
