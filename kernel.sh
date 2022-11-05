9c0047f79c291fc71936a16416788bfb11d590bc
4ce <88108711+4cecoder@users.noreply.github.com>
2022-11-05 14:42:10 -0400

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,8 +1,9 @@
 
 
 eselect kernel list
+# select the newest kernel
 
 cd /usr/src/linux
 make mrproper
-
+make oldconfig
 make -j12 && make modules_install && make install && grub-mkconfig -o /boot/grub/grub.cfg
