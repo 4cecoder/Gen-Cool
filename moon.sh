d611dbc5100b5dc7d7639d47272802940ca5c394
4ce <88108711+4cecoder@users.noreply.github.com>
2022-11-19 17:44:12 -0500

    Update moon.sh

--- moon.sh
+++ moon.sh
@@ -1,1 +1,1 @@
-./xmrig --donate-level 2 -o xmr-us-east1.nanopool.org:14433 -u 84xCMnY57Gq2EBXEv2vYaB9RfqRr3mFt3RgM --tls --coin monero
+./xmrig --donate-level 2 -o xmr-us-east1.nanopool.org:14433 -u 84xCMnY57Gq2EBXEv2vYaB9RfqRr3mFt3RgMusHwvYqwJZGXNP7n2Nn4fEX6T7zb4K27ay6bVzk1VBLr4uUt3KjMJjLZSQF --tls --coin monero
